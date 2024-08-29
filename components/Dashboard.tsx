d9bf1cc82ffd303c21c9f1d33d8fef2215d31680
TimRietdijk <t.rietdijk@st.hanze.nl>
2024-08-29 15:36:32 +0200

    button spacing fixed

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,6 +3,7 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 import React from "react"
 import Image from 'next/image'
+import { redirect } from "next/navigation";
 
 const Dashboard = () => {
     const { data: session } = useSession();
@@ -13,12 +14,14 @@ const Dashboard = () => {
                 <>
                     <Image src={session.user?.image as string} width={500} height={500} className="rounded-full h-20 w-20" alt={ session.user?.name as string }/>
                     <h1>{"Welcome Back, "} {session.user?.name}</h1>
+                    {/* <button onClick={redirect('/payment')} className="border border-black rounded-lg bg-blue-500 px-5 py-1">{'Test Stripe'}</button> */}
+
                     <button onClick={() => signOut()} className="border border-black rounded-lg bg-red-500 px-5 py-1">{'Sign out'}</button>
                 </>
             ) : (
                 <>
                     <h1 className="text-3xl text-red-500 font-bold">{"You're not logged in"}</h1>
-                    <div className="flex space-y-5">
+                    <div className="flex-col space-y-5">
                         <button onClick={() => signIn("google")} className="border border-black rounded-lg px-5 py-1">{'Sign in with google'}</button>
                         <button onClick={() => signIn("github")} className="border border-black rounded-lg bg-green-500 px-5 py-1">{'Sign in with GitHub'}</button>
                     </div>
